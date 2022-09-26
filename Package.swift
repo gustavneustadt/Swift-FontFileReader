40f8fc833479d1f667441c51ac7ef779ec64ff3b
gustavneustadt <gustav@einfach-neu.de>
2022-09-26 10:42:34 +0200

    added dependencies
    brotli and new swift collections

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FontFileReader",
+    platforms: [.macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,6 +13,11 @@ let package = Package(
             targets: ["FontFileReader"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/karlvr/Brotli", branch: "master"),
+        .package(
+            url: "https://github.com/apple/swift-collections.git",
+                .upToNextMajor(from: "1.0.3") // or `.upToNextMinor
+        )
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -20,7 +26,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FontFileReader",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Collections", package: "swift-collections"),
+                .product(name: "Brotli", package:"Brotli")
+            ]),
         .testTarget(
             name: "FontFileReaderTests",
             dependencies: ["FontFileReader"]),
--- Tests/FontFileReaderTests/FontFileReaderTests.swift
+++ Tests/FontFileReaderTests/FontFileReaderTests.swift
@@ -6,6 +6,6 @@ final class FontFileReaderTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(FontFileReader().text, "Hello, World!")
+        // XCTAssertEqual(FontFileReader().text, "Hello, World!")
     }
 }
